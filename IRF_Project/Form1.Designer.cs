8e5e8b4b2dba305f085fd5931f9a2729b5ac8766
ccuuqw <etoforevva@gmail.com>
2020-12-15 15:33:02 +0100

    Az adatbázis feltöltéséhez használt kód és elemek törlése.

--- IRF_Project/Form1.Designer.cs
+++ IRF_Project/Form1.Designer.cs
@@ -28,36 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "Pilot",
-            "Place"});
-            this.comboBox2.Location = new System.Drawing.Point(121, 56);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(121, 99);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(561, 302);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.comboBox2);
             this.Name = "Form1";
             this.ResumeLayout(false);
 
@@ -67,8 +42,6 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Button button2;
     }
 }
 
